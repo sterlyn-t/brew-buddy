aad696181665fc3185bae86fc39b5694cf48dd70
cvnd <claudiavdioneda@gmail.com>
2023-06-26 22:08:22 -0400

    add scrollabiity

--- app/src/main/java/com/example/brewbuddy/ui/theme/Generic.kt
+++ app/src/main/java/com/example/brewbuddy/ui/theme/Generic.kt
@@ -1,7 +1,10 @@
 package com.example.brewbuddy.ui.theme
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,7 +24,7 @@ fun TitleLarge(text: String) {
 
 @Composable
 fun Screen(content: @Composable () -> Unit) {
-    Surface(modifier = Modifier.fillMaxSize()) {
+    Surface(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
         content()
     }
 }
