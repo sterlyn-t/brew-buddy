0f9df016079778bef305d96a081efda30e9377cc
Wardah <wbilal@uwaterloo.ca>
2023-06-27 12:25:05 -0400

    fixed import issue

--- app/src/main/java/com/example/brewbuddy/MainActivity.kt
+++ app/src/main/java/com/example/brewbuddy/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.brewbuddy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +17,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.brewbuddy.ui.theme.BrewBuddyTheme
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,11 +48,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun NavBar() {
+    fun NavBar() {
     Row(Modifier.background(Color.Gray).fillMaxWidth().padding(16.dp)) {
         Text("Home", Modifier.padding(8.dp).clickable { /* Handle click event */ })
         Text("Profile", Modifier.padding(8.dp).clickable { /* Handle click event */ })
         Text("Settings", Modifier.padding(8.dp).clickable { /* Handle click event */ })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BrewBuddyTheme {
+        Greeting("BrewBuddy")
     }
 }
 
